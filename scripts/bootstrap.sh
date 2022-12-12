f50fadf6bae482a77c24c754312c0347ef2c85a3
Han Cen <hi@chamburr.com>
2022-12-12 11:56:49 +0800

    feat: simplify bootstrap command

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 prepare() {
-  if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: ./scripts/bootstrap.sh name address"
+  if [ -z "$1" ]; then
+    echo 'Usage: ./scripts/bootstrap.sh name'
     exit 1
   fi
 
@@ -12,8 +12,7 @@ prepare() {
 install() {
   echo 'Installing...'
 
-  ./scripts/ansible.sh playbook ansible/main.yml --tags common \
-    -l "$1" -e "ansible_host=$2"
+  ./scripts/ansible.sh playbook ansible/main.yml --tags common --limit "$1"
 
   sed -i \
     -e "s/^USER=.*/USER=$(cat ansible/group_vars/all.yml | yq '.username')/" \
@@ -22,8 +21,7 @@ install() {
 
   export NETMAKER_TOKEN=$(grep NETMAKER_TOKEN= .env | cut -d '=' -f2-)
 
-  ./scripts/ansible.sh playbook ansible/main.yml --skip-tags common \
-    -l "$1" -e "ansible_host=$2"
+  ./scripts/ansible.sh playbook ansible/main.yml --skip-tags common --limit "$1"
 }
 
 prepare "$@"
