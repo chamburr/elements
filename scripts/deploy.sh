a1b907582eba9fa7bcaa82b46542e9dc3d088dde
Han Cen <hi@chamburr.com>
2022-12-13 18:23:59 +0800

    fix: add missing backslash in script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,11 +10,11 @@ run() {
 
   hostname=$(hostname)
 
-  find common $hostname -name docker-compose.yml
+  find common $hostname -name docker-compose.yml \
     | xargs -I{} sh -c 'cd "$(dirname {})" && docker compose up --remove-orphans -d'
 
-  docker compose ls -q --all
-    | xargs -I{} sh -c 'if [ -z "$(find common '$hostname' -type d -name {})" ]; then echo {}; fi'
+  docker compose ls -q --all \
+    | xargs -I{} sh -c 'if [ -z "$(find common '$hostname' -type d -name {})" ]; then echo {}; fi' \
     | xargs -I{} sh -c 'docker compose -p {} down --remove-orphans --volumes'
 
   docker image prune --all --force
