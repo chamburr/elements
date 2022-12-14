e103c2a6c25c4eccf3d4b08217be23d8aad5534c
Han Cen <hi@chamburr.com>
2022-12-15 00:01:41 +0800

    fix: remove docker projects before creating

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,12 +13,15 @@ run() {
 
   hostname=$(hostname)
 
-  find common $hostname -name docker-compose.yml \
-    | xargs -I{} sh -c 'cd "$(dirname {})" && docker compose up --remove-orphans -d'
-
   docker compose ls -q --all \
     | xargs -I{} sh -c 'if [ -z "$(find common '$hostname' -type d -name {})" ]; then echo {}; fi' \
     | xargs -I{} sh -c 'docker compose -p {} down --remove-orphans --volumes'
+
+  find common -name docker-compose.yml \
+    | xargs -I{} sh -c 'cd "$(dirname {})" && docker compose up --remove-orphans -d'
+
+  find $hostname -name docker-compose.yml \
+    | xargs -I{} sh -c 'cd "$(dirname {})" && docker compose up --remove-orphans -d'
 
   docker image prune --all --force
 }
