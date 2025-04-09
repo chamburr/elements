d30a51f2f244f268980001a04540c73f2cfdb6a7
Han Cen <hi@chamburr.com>
2025-04-09 23:43:37 +0800

    fix: fix rclone retention

--- common/rclone/backup.sh
+++ common/rclone/backup.sh
@@ -5,8 +5,10 @@ retention=7
 hostname=$(cat /scripts/hostname)
 date=$(date +%F)
 
-find /scripts/data -type f -mtime +$(($retention - 1)) | sed 's/.\///' \
-  | xargs -I{} rclone --config /etc/rclone/rclone.conf purge minio:elements/_old/$hostname/{}
+files=$(find /scripts/data -type f -mtime +$(($retention - 1)) | sed 's/.*\///')
+
+echo $files | xargs -I{} rclone --config /etc/rclone/rclone.conf purge minio:elements/_old/$hostname/{}
+echo $files | xargs -I{} rm /scripts/data/{}
 
 rclone --config /etc/rclone/rclone.conf sync /data minio:elements/$hostname \
   --s3-no-check-bucket --backup-dir minio:elements/_old/$hostname/$date \
