8acb15c3bff2488a94cbaa1092f9eed8976737c3
Han Cen <hi@chamburr.com>
2026-09-08 21:42:57 -0700

    feat: change backup to midnight

--- common/rclone/entrypoint.sh
+++ common/rclone/entrypoint.sh
@@ -9,6 +9,6 @@ echo "$NTFY_TOKEN" > /etc/rclone/ntfy_token
 rclone --config /etc/rclone/rclone.conf config create rustfs s3 provider Minio \
   endpoint $S3_ENDPOINT access_key_id $S3_ACCESS_KEY secret_access_key $S3_SECRET_KEY
 
-echo "0 0 * * * /scripts/backup.sh" > /var/spool/cron/crontabs/root
+echo "0 7 * * * /scripts/backup.sh" > /var/spool/cron/crontabs/root
 
 exec crond -f
